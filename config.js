55a82c616c2236d9b6e1cdeee9aff6b456521645
86loading-droid <86loading@gmail.com>
2026-09-18 01:48:24 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MM_CONFIG = {
   url: "https://jtvsezcdssuvkwdyidsg.supabase.co",
-  key: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0dnNlemNkc3N1dmt3ZHlpZHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2NDg5NzYsImV4cCI6MjEwNTIyNDk3Nn0.Jpy7B3F_CHoTMcBFKk_tkUOYj_PAi1p_u4F3ZoTYViw
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0dnNlemNkc3N1dmt3ZHlpZHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2NDg5NzYsImV4cCI6MjEwNTIyNDk3Nn0.Jpy7B3F_CHoTMcBFKk_tkUOYj_PAi1p_u4F3ZoTYViw
 };
